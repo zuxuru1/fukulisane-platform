c7ff6c900e165d2c60d3e872b2fce969bec4e7d1
fukulisane mall <donlegendwear@gmail.com>
2026-08-22 06:21:05 +0200

    📦 Add schema, custom routes, and seed scripts

---
+++ prisma.config.ts
@@ -0,0 +1,7 @@
+import path from 'path'
+import { defineConfig } from 'prisma/config'
+
+export default defineConfig({
+  earlyAccess: true,
+  schema: path.join(__dirname, 'prisma', 'schema.prisma'),
+})
