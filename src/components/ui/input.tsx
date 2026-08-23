a4ef770860b0b7a4f67fced369999974b0add571
fukulisane mall <donlegendwear@gmail.com>
2026-08-23 23:12:48 +0200

    chore: add src/components/ui/input.tsx

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,5 +1,16 @@
-import * as React from 'react'
+import { type InputHTMLAttributes, forwardRef } from 'react'
 import { cn } from '@/lib/cn'
-const Input = React.forwardRef<HTMLInputElement, React.InputHTMLAttributes<HTMLInputElement>>(({ className, type, ...props }, ref) => <input type={type} className={cn('flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50', className)} ref={ref} {...props} />)
-Input.displayName = 'Input'
-export { Input }
+
+export const Input = forwardRef<HTMLInputElement, InputHTMLAttributes<HTMLInputElement>>(
+  ({ className, type, ...props }, ref) => (
+    <input
+      type={type}
+      ref={ref}
+      className={cn(
+        'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50',
+        className,
+      )}
+      {...props}
+    />
+  ),
+)
