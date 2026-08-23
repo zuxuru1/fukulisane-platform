e4875d2bf777e457b4580d35a699a3a62a68cca1
fukulisane mall <donlegendwear@gmail.com>
2026-08-23 23:10:35 +0200

    chore: add prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,7 +1,10 @@
-import path from 'path'
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (C) 2026 Shogo Technologies, Inc.
 import { defineConfig } from 'prisma/config'
 
 export default defineConfig({
-  earlyAccess: true,
-  schema: path.join(__dirname, 'prisma', 'schema.prisma'),
+  schema: 'prisma/schema.prisma',
+  datasource: {
+    url: process.env.DATABASE_URL ?? 'file:./dev.db',
+  },
 })
