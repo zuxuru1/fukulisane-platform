eb420f282962a0f5866b0f2875e15055fd6fa962
fukulisane mall <donlegendwear@gmail.com>
2026-08-23 23:12:59 +0200

    chore: add src/components/ui/separator.tsx

--- src/components/ui/separator.tsx
+++ src/components/ui/separator.tsx
@@ -1,5 +1,21 @@
-import * as React from 'react'
+import { type HTMLAttributes, forwardRef } from 'react'
 import { cn } from '@/lib/cn'
-const Separator = React.forwardRef<...>(({ className, orientation = 'horizontal', decorative = true, ...props }, ref) => <div ref={ref} role={decorative ? 'none' : 'separator'} aria-orientation={orientation || undefined} className={cn('shrink-0 bg-border', orientation === 'horizontal' ? 'h-[1px] w-full' : 'h-full w-[1px]', className)} {...props} />)
-Separator.displayName = 'Separator'
-export { Separator }
+
+interface SeparatorProps extends HTMLAttributes<HTMLDivElement> {
+  orientation?: 'horizontal' | 'vertical'
+}
+
+export const Separator = forwardRef<HTMLDivElement, SeparatorProps>(
+  ({ className, orientation = 'horizontal', ...props }, ref) => (
+    <div
+      ref={ref}
+      role="separator"
+      className={cn(
+        'shrink-0 bg-border',
+        orientation === 'horizontal' ? 'h-px w-full' : 'h-full w-px',
+        className,
+      )}
+      {...props}
+    />
+  ),
+)
